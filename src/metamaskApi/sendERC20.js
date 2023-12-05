df89c362da3f7e6b62595c7f47dfe904213b44ae
Pratham <prathamks.email@gmail.com>
2023-12-05 16:22:56 +0530

    added more coins for mainnet

--- src/metamaskApi/sendERC20.js
+++ src/metamaskApi/sendERC20.js
@@ -1,9 +1,17 @@
 import { ethers } from 'ethers';
 
-const erc20AddressMap = new Map(['0x1', '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48']);
+const erc20AddressMap = new Map([
+  '0x1',
+  {
+    usdc: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    dai: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    uni: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
+    link: '0x514910771AF9Ca656af840dff83E8264EcF986CA',
+  },
+]);
 // map of chain id to erc20 token address
 
-const tokenDecimalMap = new Map(['usdc', 6]);
+const tokenDecimalMap = new Map(['usdc', 6], ['dai', 18], ['uni', 18], ['link', 18]);
 // map of erc20tokenName to decimal
 
 const getDataField = async (address, amount, tokenAddress, tokenName) => {
@@ -52,7 +60,18 @@ export const sendERC20Token = async (address, amount, tokenName) => {
 
   Accounts = userAccounts;
 
-  const tokenAddress = erc20AddressMap.get(chain);
+  const tokenAddressesforChain = erc20AddressMap.get(chain);
+  var tokenAddress;
+
+  if (tokenName.toLowerCase() === 'usdc') {
+    tokenAddress = tokenAddressesforChain.usdc;
+  } else if (tokenName.toLowerCase() === 'dai') {
+    tokenAddress = tokenAddressesforChain.dai;
+  } else if (tokenName.toLowerCase() === 'uni') {
+    tokenAddress = tokenAddressesforChain.uni;
+  } else if (tokenName.toLowerCase() === 'link') {
+    tokenAddress = tokenAddressesforChain.link;
+  }
 
   if (Accounts.length === 0) {
     await getAccount();
