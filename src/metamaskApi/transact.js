372ed534709ecddf03e77c4d38849e60ff1ba030
Pratham <prathamks.email@gmail.com>
2023-12-05 19:20:06 +0530

    i hate async await

--- src/metamaskApi/transact.js
+++ src/metamaskApi/transact.js
@@ -3,13 +3,13 @@ import { sendTransaction } from './sendTransaction';
 import { sendERC20Token } from './sendERC20';
 
 export const transact = async (object) => {
-  object.forEach((element) => {
+  for (const element of object) {
     if (element.Tool === 'changeChain') {
-      changeChain(element.Args[0].Value);
+      await changeChain(element.Args[0].Value);
     } else if (element.Tool === 'sendTransaction') {
-      sendTransaction(element.Args[1].Value, element.Args[0].Value);
+      await sendTransaction(element.Args[1].Value, element.Args[0].Value);
     } else if (element.Tool === 'sendERC20Token') {
-      sendERC20Token(element.Args[1].Value, element.Args[0].Value, element.Args[2].Value);
+      await sendERC20Token(element.Args[1].Value, element.Args[0].Value, element.Args[2].Value);
     }
-  });
+  }
 };
