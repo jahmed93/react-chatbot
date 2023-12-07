2b04dc54362c938602be9757ed031cfb91a92203
Pratham <prathamks.email@gmail.com>
2023-12-07 13:52:34 +0530

    remove 1inch

--- src/index.js
+++ src/index.js
@@ -3,23 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ReactGA from 'react-ga4';
-import { MetaMaskProvider } from '@metamask/sdk-react';
 
 ReactGA.initialize(`${process.env.REACT_APP_GOOGLE_ANALYTICS_KEY}`);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MetaMaskProvider
-      debug={false}
-      sdkOptions={{
-        checkInstallationImmediately: false,
-        dappMetadata: {
-          name: 'Demo React App',
-          url: window.location.host,
-        },
-      }}
-    >
-      <App />
-    </MetaMaskProvider>
+    <App />
   </React.StrictMode>,
 );
