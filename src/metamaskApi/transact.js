415f2dcc5a89eb918bc5f7ca7a20b1303db6f0b9
Ishan <gargishan.1211@gmail.com>
2023-12-10 02:29:14 +0530

    tools

--- src/metamaskApi/transact.js
+++ src/metamaskApi/transact.js
@@ -6,6 +6,7 @@ import { gas1inch } from './gas1inch';
 import { balance1inch } from './getBalances';
 import { price1inch } from './price1inch';
 import { callContractFunction } from './callContractFunction';
+import { gasApi } from './gasApi';
 
 export const transact = async (object) => {
   for (const element of object) {
@@ -17,13 +18,13 @@ export const transact = async (object) => {
       await sendERC20Token(element.Args[1].Value, element.Args[0].Value, element.Args[2].Value);
     } else if (element.Tool === 'swapCurrency') {
       await swap(element.Args[0].Value, element.Args[1].Value, element.Args[2].Value);
-    } else if (element.Tool === 'gas1inch') {
+    } else if (element.Tool === 'getgasprice1inch') {
       await gas1inch(element.Args[0].Value);
     } else if (element.Tool === 'getBalance') {
       await balance1inch();
-    } else if (element.Tool === 'price1inch') {
+    } else if (element.Tool === 'getprice') {
       await price1inch(element.Args[0].Value, element.Args[1].Value);
-    } else if (element.Tool === 'gasApi') {
+    } else if (element.Tool === 'getgaspriceMetamask') {
       await gasApi(element.Args[0].Value);
     } else if (element.Tool === 'callContractFunction') {
       await callContractFunction(
